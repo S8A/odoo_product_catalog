8b98a556432b386ff2037b6158ae2bada7ca4972
S8A <samuelochoap@gmail.com>
2024-02-26 13:18:04 -0400

    Create product catalog addon

---
+++ s8a_product_catalog/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Product Catalog",
+    "summary": "Simple product catalog web template, for printing.",
+    "description": """
+        Simple product catalog web template, for printing.
+    """,
+    "author": "Samuel Ochoa",
+    "maintainer": "Samuel Ochoa",
+    "website": "https://s8a.github.io",
+    "category": "Extra Tools",
+    "version": "0.1",
+    "application": False,
+    "license": "LGPL-3",
+    "depends": [
+        "product",
+    ],
+    "data": [
+    ],
+}
